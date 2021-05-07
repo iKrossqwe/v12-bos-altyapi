81b45bd080892e8fd956052f7ffd910054ccb44d
iKrossqwe <83454822+iKrossqwe@users.noreply.github.com>
2021-05-07 22:54:37 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,21 +1,16 @@
-const ayarlar = require("../ayarlar.json");
-const db = require("quick.db");
+const db = require('quick.db')
+let Discord = require("discord.js")
+const { Client, Util } = require("discord.js");
 
-let talkedRecently = new Set();
+const ayarlar = require('../ayarlar.json');
 module.exports = async message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-  setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  });
+          let prefix = await require("quick.db").fetch(`prefix.${message.guild.id}`) || ayarlar.prefix
+
   let client = message.client;
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || ayarlar.prefix;
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0].slice(prefix.length);
-  let params = message.content.split(" ").slice(1);
+  let command = message.content.split(' ')[0].slice(prefix.length);
+  let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
   if (client.commands.has(command)) {
@@ -23,8 +18,39 @@ module.exports = async message => {
   } else if (client.aliases.has(command)) {
     cmd = client.commands.get(client.aliases.get(command));
   }
+  
+  
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
-};
+  
+
+
+     
+      
+if(cmd) {
+  if(cmd.conf.enabled == false) {
+          if (!ayarlar.sahip.includes(message.author.id) && !ayarlar.sahip.includes(message.author.id)) {
+               const embed1 = new Discord.MessageEmbed()
+                    .setDescription(`:x: **${cmd.help.name}** isimli komut şuanda geçici olarak kullanıma kapalıdır!`)
+                    .setColor("#09aee5")
+                message.channel.send(embed1)
+                return
+            
+          }
+  }
+}
+if (!client.commands.has(command)) {
+   if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+   } else {
+   if(command == '') return;
+     message.channel.send(
+      new Discord.MessageEmbed()
+       .setColor(#RANDOM)
+   .setDescription("**Botta __" + command + '__ Adında Bir Komut Yok?!**'))
+  }
+}
+     }
+ 
