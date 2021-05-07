ba53715ebd9d5068227954fc116a87b459252247
iKrossqwe <83454822+iKrossqwe@users.noreply.github.com>
2021-05-07 22:42:53 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,122 @@
+const discord = require("discord.js");
+const fs = require("fs");
+const http = require("http");
+const db = require("quick.db");
+const moment = require("moment");
+const express = require("express");
+const ayarlar = require("./ayarlar.json");
+const app = express();
+app.get("/", (request, response) => {
+  console.log(`7/24 Hizmet Vermekteyim!`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//-----Ready.js | .gg/pMr8twXsbv-----\\
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", async () => {
+  client.appInfo = await client.fetchApplication();
+  setInterval(async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+
+  client.user.setActivity(`iKrossqwe | Abone Ol🎈`, { type: "PLAYING" });
+
+  console.log("Bot Başarıyla Aktif<3");
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require("./util/eventLoader.js")(client);
+
+//-----ready.js son |.gg/pMr8twXsbv-----\\
+
+
+//-----Komut Algılıyıcısı | .gg/pMr8twXsbv-----\\
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+//-----Komut Algılıyıcısı Son | .gg/pMr8twXsbv-----\\
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+client.login(ayarlar.token);
+
+//-----komutlar aşağı | .gg/pMr8twXsbv-----\\
